064bff50fe2b0df78fd9ab4e77f149a4f1d0a76a
Narfinger <Narfinger@users.noreply.github.com>
2024-07-18 14:43:47 +0900

    add client option for all

--- client/src/main.rs
+++ client/src/main.rs
@@ -12,26 +12,42 @@ pub(crate) struct MenuItem {
     pub(crate) duration_in_minutes: u64,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+/// A table in the restaurant having various menuitems
+pub(crate) struct Table {
+    pub(crate) table_number: usize,
+    pub(crate) items: Vec<MenuItem>,
+}
+
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
+/// Argument Parsing
 struct Args {
-    #[clap(short, long, value_parser, num_args = 1..,value_delimiter = ' ')]
-    add: Option<Vec<u64>>,
+    /// Limit the number of results
     #[clap(short, long)]
+    limit: Option<usize>,
+
+    #[clap(short, long, value_parser, num_args = 2..,value_delimiter = ' ', group="input")]
+    /// add menu items to a table, given as `table_number menu_item1 menu_item2...`
+    add: Option<Vec<usize>>,
+
+    /// delete a menu item given as `table_number menu_item1 menu_item2...`
+    #[clap(short, long, value_parser, num_args = 2..,value_delimiter = ' ', group="input")]
+    delete: Option<Vec<usize>>,
+
+    /// get all menuitems
+    #[clap(long, group = "input")]
+    all: bool,
+
+    /// get the current menu items for a table
+    #[clap(short, long, group = "input")]
     get: Option<usize>,
 }
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    if let Some(i) = args.get {
-        let menu_items =
-            reqwest::blocking::get(format!("http://127.0.0.1:3000/{}/?key={}", i, API_KEY))?
-                .json::<Vec<MenuItem>>()?;
-        println!("--------Showing Items for table {}----------", i);
-        for i in menu_items {
-            println!("Item#: {} Time: {}", i.item_number, i.duration_in_minutes);
-        }
-    } else if let Some(mut add_vec) = args.add {
+    // add
+    if let Some(mut add_vec) = args.add {
         let menu_items = add_vec.split_off(1);
         let cl = reqwest::blocking::Client::new();
         cl.post(format!(
@@ -41,6 +57,54 @@ fn main() -> anyhow::Result<()> {
         ))
         .json(&menu_items)
         .send()?;
+    // delete
+    } else if let Some(mut del_vec) = args.delete {
+        let menu_items = del_vec.split_off(1);
+        let cl = reqwest::blocking::Client::new();
+        for _ in menu_items {
+            cl.delete(format!(
+                "http://127.0.0.1:3000/{}/?key={}",
+                del_vec.first().unwrap(),
+                API_KEY
+            ))
+            .send()?;
+        }
+    // all
+    } else if args.all {
+        let query_string = if let Some(l) = args.limit {
+            format!("http://127.0.0.1:3000/?key={}&limit={}", API_KEY, l)
+        } else {
+            format!("http://127.0.0.1:3000/?key={}", API_KEY,)
+        };
+        let tables = reqwest::blocking::get(query_string)?.json::<Vec<Table>>()?;
+        for i in tables {
+            println!(
+                "--------Showing Items for table {}----------",
+                i.table_number
+            );
+            for (index, menu_item) in i.items.iter().enumerate() {
+                println!(
+                    "{} | Item#: {} Time: {}",
+                    index, menu_item.item_number, menu_item.duration_in_minutes
+                );
+            }
+        }
+    // get
+    } else if let Some(i) = args.get {
+        let query_string = if let Some(l) = args.limit {
+            format!("http://127.0.0.1:3000/{}/?key={}&limit={}", i, API_KEY, l)
+        } else {
+            format!("http://127.0.0.1:3000/{}/?key={}", i, API_KEY,)
+        };
+        let menu_items = reqwest::blocking::get(query_string)?.json::<Vec<MenuItem>>()?;
+
+        println!("--------Showing Items for table {}----------", i);
+        for (inndex, menu_item) in menu_items.iter().enumerate() {
+            println!(
+                "{} | Item#: {} Time: {}",
+                index, menu_item.item_number, menu_item.duration_in_minutes
+            );
+        }
     }
 
     Ok(())
